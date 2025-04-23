bb835d78b07b5c86f7585066604cf98ac6a7987c
shannsks <shannu.sks@gmail.com>
2025-04-23 12:50:17 -0400

    added

--- child_module/storage_account/sa.tf
+++ child_module/storage_account/sa.tf
@@ -15,3 +15,22 @@ resource "azurerm_storage_account" "storage_account" {
     account_replication_type = "LRS"
   
 }
+
+resource "azurerm_storage_account" "storage_account" {
+    name = var.saname
+    location = var.rglocation
+    resource_group_name = var.rgname
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+  
+}
+
+resource "azurerm_storage_account" "storage_account22" {
+    name = var.saname
+    location = var.rglocation
+    resource_group_name = var.rgname
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+  
+}
+
