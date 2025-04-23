452be58da7843850387da82ec4e502eefb5d9c77
SOMNATH VERMA <somverma.earth@outlook.com>
2025-04-23 02:13:24 -0400

    Update rg.tf

--- child_module/resource_group/rg.tf
+++ child_module/resource_group/rg.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rg_ame" {
   name = var.rgname
   location = var.rglocation
 }
+resource "azurerm_resource_group" "rg_ame1" {
+  name = var.rgname
+  location = var.rglocation
+}
