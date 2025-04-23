cfa13c2450c892e9539e8a8b05ab286ee2a8a0f6
shannsks <shannu.sks@gmail.com>
2025-04-23 03:43:36 -0400

    stg addnig

--- child_module/storage_account/sa.tf
+++ child_module/storage_account/sa.tf
@@ -6,3 +6,12 @@ resource "azurerm_storage_account" "storage_account" {
     account_replication_type = "LRS"
   
 }
+
+resource "azurerm_storage_account" "storage_account" {
+    name = var.saname
+    location = var.rglocation
+    resource_group_name = var.rgname
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+  
+}
