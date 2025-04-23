d222a95e92db417c5d40802bde2f5ca8a4d538e6
SOMNATH VERMA <somverma.earth@outlook.com>
2025-04-23 02:17:58 -0400

    added 1 rg

--- child_module/resource_group/rg.tf
+++ child_module/resource_group/rg.tf
@@ -6,3 +6,8 @@ resource "azurerm_resource_group" "rg_ame1" {
   name = var.rgname
   location = var.rglocation
 }
+
+resource "azurerm_resource_group" "rg_ame2" {
+  name = var.rgname
+  location = var.rglocation
+}
