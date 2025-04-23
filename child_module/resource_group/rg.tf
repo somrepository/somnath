fe6b16cdcb23ce00ebde0c20cd7fd34fca630483
shannsks <shannu.sks@gmail.com>
2025-04-23 03:03:10 -0400

    added a shannu rg

--- child_module/resource_group/rg.tf
+++ child_module/resource_group/rg.tf
@@ -6,3 +6,8 @@ resource "azurerm_resource_group" "rg_ame1" {
   name = var.rgname
   location = var.rglocation
 }
+
+resource "azurerm_resource_group" "rg_shannu1" {
+  name = var.rgname
+  location = var.rglocation
+}
