2ea782af4061a163a20e32b8450ad89fae2f4e67
shannsks <shannu.sks@gmail.com>
2025-04-23 02:31:47 -0400

    removed RG

--- child_module/resource_group/rg.tf
+++ child_module/resource_group/rg.tf
@@ -6,8 +6,3 @@ resource "azurerm_resource_group" "rg_ame1" {
   name = var.rgname
   location = var.rglocation
 }
-
-resource "azurerm_resource_group" "rg_ame2" {
-  name = var.rgname
-  location = var.rglocation
-}
